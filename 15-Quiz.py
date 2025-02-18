956ca7435f81f854865edffc85eb2ba742006777
Luis Gustavo Barbosa Oliveira <gustavokreft@gmail.com>
2025-02-18 16:13:05 -0300

    Início Quiz

---
+++ 15-Quiz.py
@@ -0,0 +1,16 @@
+perguntas=(("1. Qual é o maior planeta do Sistema Solar?"),
+            ("2. Quem pintou a Mona Lisa?"),
+           ("3. Qual é o elemento químico mais abundante na crosta terrestre?"),
+           ("4. Em que ano o homem pisou na Lua pela primeira vez?"),
+           ("5. Qual é o maior oceano do mundo?"))
+    
+for linhas in perguntas:
+    print("\n")
+    for itens in linhas:
+        print(itens, end="")#Finalizar o restante
+
+    
+
+
+
+
